6e38c2d8bf9d60e657605d2a850e95b035bf86a9
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-02-07 20:43:37 +0100

    tech: `$ yarn prettier:fix`

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="global-layout">
-    <header style="background-color: #DDD">
-      <router-link to="/">{{ $site.title }}</router-link>· <router-link to="/tag/">Tag</router-link>
-    </header><br>
-    <DefaultGlobalLayout/><br>
-    <footer style="background-color: #DDD"><a href="https://github.com/ulivz/70-lines-of-vuepress-blog-theme">Github</a> · Powered by VuePress
-</footer>
+    <header style="background-color: #ddd">
+      <router-link to="/">{{ $site.title }}</router-link
+      >· <router-link to="/tag/">Tag</router-link>
+    </header>
+    <br />
+    <DefaultGlobalLayout /><br />
+    <footer style="background-color: #ddd">
+      <a href="https://github.com/ulivz/70-lines-of-vuepress-blog-theme"
+        >Github</a
+      >
+      · Powered by VuePress
+    </footer>
   </div>
 </template>
 
 <script>
-  import GlobalLayout from '@app/components/GlobalLayout.vue'
+import GlobalLayout from '@app/components/GlobalLayout.vue';
 
-  export default {
-    components: { DefaultGlobalLayout: GlobalLayout },
-  }
+export default {
+  components: { DefaultGlobalLayout: GlobalLayout },
+};
 </script>
