1846e55eaecb0c28ec9ce8d05f33ff19a3fb8f58
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-02-11 19:49:56 +0100

    tech: Vue.js tests

--- theme/components/Footer.vue
+++ theme/components/Footer.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>myFooter</div>
+  <div @click="roro">myFooter</div>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'Footer',
+  // eslint-disable-next-line
+  data() {
+    return {
+      toto: false,
+    };
+  },
+  methods: {
+    // eslint-disable-next-line
+    roro() {
+      // eslint-disable-next-line no-console
+      console.log(this.toto);
+    },
+  },
+};
+</script>
