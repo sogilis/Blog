e642d12938e3bfd0bc844def82fffe2fce9d006d
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-02-14 21:42:18 +0100

    feat: install some cool plugins of VuePress + upgrade dependencies

--- theme/index.js
+++ theme/index.js
@@ -1,35 +1,56 @@
+const vuePressBlog = [
+  '@vuepress/blog',
+  {
+    directories: [
+      {
+        id: 'post',
+        dirname: '_posts',
+        path: '/',
+        pagination: {
+          perPagePosts: 2,
+        },
+      },
+    ],
+    frontmatters: [
+      {
+        id: 'tags',
+        keys: ['tags', 'tags'],
+        path: '/tags/',
+        layout: 'Tags',
+        frontmatter: { title: 'Tags' },
+        itemlayout: 'Tags',
+        pagination: {
+          perPagePosts: 3,
+        },
+      },
+    ],
+  },
+];
+
+const mediumZoon = [
+  // Official plugins is buggy. It works only for the first page loaded
+  // When we follow an internal link (trigger SPA navigatione)
+  // it does not appear.
+  // '@vuepress/medium-zoom',
+  'vuepress-plugin-medium-zoom',
+  {
+    selector: '#articlepage img',
+  },
+];
+
 module.exports = {
   plugins: [
     'typescript',
     // A very simple plugin
     'vuepress-plugin-reading-time',
-    [
-      '@vuepress/blog',
-      {
-        directories: [
-          {
-            id: 'post',
-            dirname: '_posts',
-            path: '/',
-            pagination: {
-              perPagePosts: 2,
-            },
-          },
-        ],
-        frontmatters: [
-          {
-            id: 'tags',
-            keys: ['tags', 'tags'],
-            path: '/tags/',
-            layout: 'Tags',
-            frontmatter: { title: 'Tags' },
-            itemlayout: 'Tags',
-            pagination: {
-              perPagePosts: 3,
-            },
-          },
-        ],
-      },
-    ],
+
+    // '@vuepress/plugin-nprogress',
+
+    mediumZoon,
+
+    '@vuepress/back-to-top',
+
+    vuePressBlog,
+    'smooth-scroll',
   ],
 };
