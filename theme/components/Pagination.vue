404a28711f9d0af3245af89a5b14b508366015d8
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-02-16 21:32:17 +0100

    feat: add pagination

--- theme/components/Pagination.vue
+++ theme/components/Pagination.vue
@@ -1,9 +1,29 @@
 <template>
-  <div id="pagination">
-    <RouterLink v-if="pagination.hasPrev" :to="pagination.prevLink"
+  <div class="pagination">
+    <RouterLink
+      v-if="pagination.hasPrev"
+      class="pagination__prev"
+      :to="pagination.prevLink"
       >Prev</RouterLink
     >
-    <RouterLink v-if="pagination.hasNext" :to="pagination.nextLink"
+    <span
+      v-for="pageIndex in pagination._paginationPages.length"
+      :key="pageIndex"
+      class="pagination__allpages"
+    >
+      <RouterLink
+        v-if="pageIndex !== 1"
+        class="page-link"
+        :to="'/page/' + pageIndex"
+      >
+        {{ pageIndex }}
+      </RouterLink>
+      <RouterLink v-else :to="'/'"> 1 </RouterLink>
+    </span>
+    <RouterLink
+      v-if="pagination.hasNext"
+      class="pagination__next"
+      :to="pagination.nextLink"
       >Next</RouterLink
     >
   </div>
@@ -18,5 +38,47 @@ export default {
       required: true,
     },
   },
+  data(): { currentPage: number } {
+    return {
+      currentPage: 1,
+    };
+  },
+  mounted(): void {
+    // Is undefined when HTML is rendered
+    if (this.pagination._currentPage) {
+      this.currentPage = this.getCurrentPage(this.pagination._currentPage.path);
+    }
+  },
+  updated(): void {
+    // Is undefined when HTML is rendered
+    if (this.pagination._currentPage) {
+      this.currentPage = this.getCurrentPage(this.pagination._currentPage.path);
+    }
+  },
+  methods: {
+    getCurrentPage(currentPagePath: string): number {
+      if (currentPagePath !== '/') {
+        return Number(currentPagePath.substring(6, currentPagePath.length - 1));
+      }
+      return 1;
+    },
+  },
 };
 </script>
+
+<style lang="css" scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 3rem;
+}
+
+.pagination__allpages .router-link-exact-active {
+  color: orange;
+}
+
+.pagination__allpages {
+  padding: 1rem;
+  display: inline-block;
+}
+</style>
