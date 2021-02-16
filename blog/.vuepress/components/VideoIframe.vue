10aa88f740412f3b1ae4fc6b642e347ee5c648e6
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-02-16 15:47:44 +0100

    feat: add component VideoIframe as https://gohugo.io/content-management/shortcodes/#youtube

---
+++ blog/.vuepress/components/VideoIframe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="videoiframe">
+    <slot> </slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'VideoIframe',
+};
+</script>
+
+<style lang="css" scoped>
+.videoiframe {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+  border: 0;
+}
+</style>
